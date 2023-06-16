6afef417c967cb1e011ac0cb9eb31e32c92d6d46
angelaseibert <92227575+angelaseibert@users.noreply.github.com>
2023-06-16 14:58:36 -0600

    Add files via upload

--- results_analysis_functions.py
+++ results_analysis_functions.py
@@ -3,7 +3,8 @@
 import os
 import pandas as pd
 import statsmodels.api as sma
-boxplot_palette = ['#ff7f00','#377eb8','#984ea3']
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 def stack_its(file_path, final_csv_name):
     """
@@ -32,7 +33,7 @@ def stack_its(file_path, final_csv_name):
     final_df = pd.concat(df_list, ignore_index = True)
     return final_df.to_csv(file_path + final_csv_name, index = False)
 
-def load_and_filter(results_path, column_name, column_name_type):
+def load_and_filter(results_path, column_name, column_name_type, exclude = False):
     """
     Perform an ordinary least squares regression analysis on data
     
@@ -52,7 +53,17 @@ def load_and_filter(results_path, column_name, column_name_type):
     summary of the regression results
     """
     df_unfiltered = pd.read_csv(results_path)
-    df_filtered = df_unfiltered[df_unfiltered[column_name] == column_name_type]
+    if exclude:
+        df_filtered = df_unfiltered[df_unfiltered[column_name] != column_name_type]
+    else:
+        df_filtered = df_unfiltered[df_unfiltered[column_name] == column_name_type]
+    return df_filtered
+
+def filter_df(dataframe, column_name, column_name_type, exclude = False):
+    if exclude:
+        df_filtered = dataframe[dataframe[column_name] != column_name_type]
+    else:
+        df_filtered = dataframe[dataframe[column_name] == column_name_type]
     return df_filtered
 
 def regression_analysis(reg_data, ind_var_name, dep_var_name):
@@ -82,6 +93,90 @@ def regression_analysis(reg_data, ind_var_name, dep_var_name):
     
     return print(_2.summary())
     
-def box_plots()
+def box_plots(box_data, box_palette):
+    """
+    Plot boxplots of LAI data
+    
+    Parameters
+    ----------------
+    box_data:
+        the dataframe to generate a boxplot with
+    box_palette:
+        the palette you would like to use to color the boxes
+        type: series of color code strings
+    Returns
+    ----------------
+    box plot of the LAI over time, grouped by carbon dioxide
+    """
+    plt.figure(dpi= 1200)
+    LAI_plot = sns.boxplot(data=box_data, x="year", y= 'LAI', hue = 'carbon dioxide', palette = box_palette)
+    plt.xlabel("Years", fontsize = 14)
+    plt.ylabel("LAI (m${}^2$/m${}^2$)", fontsize = 14)
+    sns.move_legend(LAI_plot, "best")
+    return LAI_plot
+    
+def kde_plots(kde_data):
+    """
+    Plot kernel density functions on top of each other
+    
+    Parameters
+    ----------------
+    kde_data:
+        the dataframe to generate the distributions
+    Returns
+    ----------------
+    kernel density functions showing the distributions of LAI 
+    """
+    plt.figure(dpi= 1200)
+    df_LAIall_ec = kde_data[kde_data['carbon dioxide'] == 'elevated']
+    df_LAIall_ac = kde_data[kde_data['carbon dioxide'] == 'ambient']
+    df_LAIall_control = kde_data[kde_data['carbon dioxide'] == 'control']
+    
+    kde_ec_plot = sns.kdeplot(data=df_LAIall_ec['LAI'], color = '#ff7f00', fill = True, label = 'Elevated CO${}_2$ & Elevated Temp')
+    kde_ac_plot = sns.kdeplot(data=df_LAIall_ac['LAI'], color = '#377eb8', fill = True, label = 'Ambient CO${}_2$ & Elevated Temp')
+    kde_c_plot = sns.kdeplot(data=df_LAIall_control['LAI'], color = '#984ea3', fill = True, label = 'Control')
 
+    plt.xlabel("LAI (m${}^2$/m${}^2$)", fontsize = 14)
+    plt.ylabel("Frequency", fontsize = 14)
+    plt.legend()
+    
+    print('Elevated CO2:', (df_LAIall_ec['LAI']).describe()) 
+    print('Ambient CO2:', (df_LAIall_ac['LAI']).describe()) 
+    print('Control:', (df_LAIall_control['LAI']).describe())
+    
+    return plt
+
+def LAI_hist_plot(hist_data):
+    """
+    Plot histogram of all LAI values
+    
+    Parameters
+    ----------------
+    hist_data:
+        the dataframe to generate the distributions
+    Returns
+    ----------------
+    histogram showing the distribution of LAI 
+    """
+    plt.figure(dpi = 1200)
+    LAI_histogram = sns.histplot(data=hist_data['LAI'], binwidth=0.2, kde=True, stat = 'probability', color = '#e41a1c', label = 'Plot LAI')
+    plt.xlabel("LAI (m${}^2$/m${}^2$)", fontsize = 14)
+    plt.ylabel("Frequency", fontsize = 14)
+    plt.legend()
+    
+    print((hist_data['LAI']).describe());
+    
+    return LAI_histogram
+
+def LAI_lineplot(line_data, line_palette):
+    plt.figure(dpi= 1200, figsize = (10,6))
+    LAI_species_lineplot = sns.lineplot(data=line_data, x="year", y= 'LAI', hue = 'species', 
+                                        style = 'carbon dioxide', palette = line_palette, marker = 'o', err_style = 'bars', markersize= 8)
+    plt.xlabel("Years", fontsize = 14)
+    plt.ylabel("LAI (m${}^2$/m${}^2$)", fontsize = 14)
+    sns.move_legend(LAI_species_lineplot, "best")
+    return LAI_species_lineplot
+
+#FIGURES USING TEMPERATURES NEED TO BE FIXED
+ 
 
