00ac742b1d8db4ac673cf25eff4a777939b82ec6
angelaseibert <92227575+angelaseibert@users.noreply.github.com>
2023-07-13 16:18:46 -0600

    Add files via upload

--- results_analysis_functions.py
+++ results_analysis_functions.py
@@ -5,6 +5,9 @@ import pandas as pd
 import statsmodels.api as sma
 import matplotlib.pyplot as plt
 import seaborn as sns
+import warnings
+from sklearn.preprocessing import MinMaxScaler
+
 
 def stack_its(file_path, final_csv_name):
     """
@@ -218,6 +221,65 @@ def LAI_lineplot(line_data, line_palette):
     sns.move_legend(LAI_species_lineplot, "best")
     return LAI_species_lineplot
 
-#FIGURES USING TEMPERATURES NEED TO BE FIXED
- 
+def normal_data(data_to_norm, filter_by_temp, what_temp, filter_by_co2, what_co2, cols_norm_name, file_outpath, csv_name):
+    """
+    Normalize a dataframe using the MinMaxScaler function from sklearn. Each group of LAI values is normalized by plot or the unique treatment conditions.
+    
+    Parameters
+    ----------------
+    data_to_norm:
+        the dataframe full of the data to normalize
+    type:
+        pandas dataframe
+    filter_by_temp:
+        the name of the temperature column, you could also go by a different column name if you wanted to normalize by something else
+    type:
+        string, ex: 'temp'
+    what_temp: 
+        the category of the data that it should be filtered by
+    type: 
+        string or number, ex: '0' or 0, just depends on the original df
+    filter_by_co2:
+        the name of the co2 treatment column
+    type: 
+        string
+    what_co2:
+        the category of the data in the co2 treatment column 
+    type:
+        string, ex: 'elevated' or 'ambient'
+    cols_norm_name: 
+        the column name that contains the data you want to normalize
+    type:
+        string, ex 'LAI'
+    file_outpath:
+        filepath to output the csv file
+    type:
+        string containing a file path
+    csv_name:
+        name of the csv file you want to call the results 
+    type:
+        string with the extension .csv
+    Returns
+    ----------------
+    csv file containing LAI data normalized by plot or treatment group 
+    """
+    filtered_1 = filter_df(data_to_norm, filter_by_temp, what_temp, exclude = False)
+    temp_df = filter_df(filtered_1, filter_by_co2, what_co2, exclude = False)
+    cols_to_norm = [cols_norm_name]
+    temp_df[cols_to_norm] = MinMaxScaler().fit_transform(temp_df[cols_to_norm])
+    warnings.filterwarnings('ignore')
+    return temp_df.to_csv(file_outpath + csv_name, index = False)
+
+def LAInormal_timeseries (normal_df, legend_title, line_palette):
+    plt.figure(dpi= 1200, figsize = (10,6))
+    LAI_normtimeseries = sns.lineplot(data=normal_df, x="year", y= 'LAI', 
+                                      hue_order = ['0','2.25','4.5','6.75','9','control'], hue = 'temp', palette = line_palette)
+    plt.ylabel("Normalized Leaf Area Index (m${}^2$/m${}^2$)", fontsize = 14);
+    plt.xlabel("Year", fontsize = 14);
+    plt.legend(title= legend_title, loc='best')
+    plt.plot()
+    plt.ylim(0, 1)
+    return LAI_normtimeseries
+
+
 
